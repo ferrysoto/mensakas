51c4087c15d603edd5e44c4c0853f7d2c54f8e1a
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-02-19 01:03:27 +0100

    dashboard updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,15 +4,34 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">
+                  <i class="fas fa-shopping-cart icon-right"></i>
+                  Last Orders
+                </div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-                    You are logged in!
+                    <table class="table table-responsive-md text-left">
+                      <tr>
+                        <td class="align-middle non-border"><b>Reference order</b></td>
+                        <td class="align-middle non-border"><b>Address delivered</b></td>
+                        <td class="align-middle non-border"><b>Price</b></td>
+                        <td class="align-middle non-border"><b>Invoice Num</b></td>
+                        <td class="align-middle non-border"><b>Date</b></td>
+                      </tr>
+                      <tr>
+                        @foreach($orders as $order)
+                          <td class="align-middle non-border">{{$order->id_order}}</td>
+                          <td class="align-middle non-border">{{$order->address}}</td>
+                          <td class="align-middle non-border">{{$order->total_paid}}</td>
+                          <td class="align-middle non-border">{{$order->invoice_num}}</td>
+                          <td class="align-middle non-border">{{$order->invoice_date}}</td>
+                        @endforeach
+                      </tr>
+                    </table>
                 </div>
             </div>
         </div>
