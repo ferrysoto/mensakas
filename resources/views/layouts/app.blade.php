4312b49d9edfc906b297017a57b220a0c237025d
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-02-19 02:00:53 +0100

    js new file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
       <!-- Scripts -->
       <script src="{{ asset('js/app.js') }}" defer></script>
+      <script	src="{{ asset('js/custom.js') }}"></script>
       <script src="{{ asset('js/layout/sidebar.js') }}" defer></script>
 
       <!-- Fonts -->
