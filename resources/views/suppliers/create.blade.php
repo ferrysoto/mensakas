a559aa6ebc9b44928322b4a6a9bc37b1ff47a185
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-02-20 12:47:20 +0100

    fix some fields to supplier register form

--- resources/views/suppliers/create.blade.php
+++ resources/views/suppliers/create.blade.php
@@ -20,23 +20,24 @@
                     @endforeach
                   </select>
                 </div>
-                <div class="form-group col-md-2">
-                  <label for="supplierName">Name</label>
-                  <input type="text" class="form-control" id="supplierName" name="name" value="" maxlength="255" placeholder="Leandro" required>
+                <div class="form-group col-md-3">
+                  <label for="supplierName">Full name</label>
+                  <input type="text" class="form-control" id="supplierName" name="name" value="" maxlength="255" placeholder="Leandro Mieza" required>
                 </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
+                <div class="form-group col-md-4">
                   <label for="supplierEmail">Email</label>
                   <input type="email" class="form-control" id="supplierEmail" name="email" placeholder="email@domain.com" maxlength="150" required>
                 </div>
-                <div class="form-group col-md-6">
+                <div class="form-group col-md-3">
                   <label for="supplierPhone">Phone</label>
                   <input type="number" class="form-control" id="supplierPhone" name="phone" placeholder="699 999 999" min="600000000" max="999999999" required>
                 </div>
               </div>
-              <div class="form-group col-md-6">
-                <label for="supplierAddress">Address</label>
-                <input type="text" class="form-control" id="supplierAddress" name="address" placeholder="1234 Main St" maxlength="128" required>
+              <div class="form-row">
+                <div class="form-group col-md-12">
+                  <label for="supplierAddress">Address</label>
+                  <input type="text" class="form-control" id="supplierAddress" name="address" placeholder="1234 Main St" maxlength="128" required>
+                </div>
               </div>
               <div class="form-row">
                 <div class="form-group col-md-6">
@@ -57,22 +58,24 @@
                   <input type="number" class="form-control" id="supplierZip" name="zipcode" min="00001" max="99999" placeholder="00000" required>
                 </div>
               </div>
-              <div class="form-group col-md-3">
-              <div class="form-group ">
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" id="passReset">
-                  <label class="form-check-label" for="passReset">
-                    Send welcome email
-                  </label>
+              <div class="form-row">
+                <div class="form-group col-md-9">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="passReset">
+                    <label class="form-check-label" for="passReset">
+                      Send welcome email
+                    </label>
+                  </div>
                 </div>
-              </div>
-              <div class="float-right col-md-6">
-                <button type="submit" class="btn btn-primary">Add supplier</button>
+                <div class="form-group col-md-3">
+                  <div class="float-right">
+                    <button type="submit" class="btn btn-primary">Add supplier</button>
+                  </div>
+                </div>
               </div>
             </form>
           </div>
         </div>
       </div>
     </div>
-  </div>
 @endsection
